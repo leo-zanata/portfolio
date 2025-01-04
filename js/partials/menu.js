5b16f71041d78978b19326e8f2bab8acda6cc596
LZanata <leonardozana19@gmail.com>
2025-01-04 18:45:16 -0300

    commit

---
+++ js/partials/menu.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('menu').innerHTML = `
+      <div class="container">
+        <div class="container-main">
+          <div class="main-container">
+            <div class="main-content">
+              <a href="home.html"> <i class="bi bi-house"></i> Início </a>
+            </div>
+          </div>
+  
+          <div class="main-container">
+            <div class="main-content">
+              <a href="about.html">
+                <i class="bi bi-file-person-fill"></i> Sobre
+              </a>
+            </div>
+          </div>
+  
+          <div class="main-container">
+            <div class="main-content">
+              <a href="abilities.html"> <i class="bi bi-person-gear"></i> Habilidades </a>
+            </div>
+          </div>
+  
+          <div class="main-container">
+            <div class="main-content">
+              <a href="projects.html">
+                <i class="bi bi-lightbulb"></i> Projetos
+              </a>
+            </div>
+          </div>
+  
+          <div class="main-container">
+            <div class="main-content">
+              <a href="experience.html">
+                <i class="bi bi-journal-text"></i> Experiências
+              </a>
+            </div>
+          </div>
+  
+          <div class="main-container">
+            <div class="main-content">
+              <a href="contact.html">
+                <i class="bi bi-people-fill"></i> Contato
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+  
